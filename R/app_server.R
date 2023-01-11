feaf5afd5a7c81066907c97927ba1793c6629ad9
AuBony <audrey.bony@yahoo.fr>
2023-01-11 13:54:27 +0100

    2.1
    Amélioration du tableau ANOVA

--- R/app_server.R
+++ R/app_server.R
@@ -2,13 +2,15 @@
 #'
 #' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
-#' @import shiny readxl ggplot2 FactoMineR
+#' @import shiny readxl ggplot2 FactoMineR factoextra dplyr
 #' @importFrom dplyr filter
 #' @noRd
 app_server <- function(input, output, session) {
 
   #DATA
   dta <-  read_xlsx("inst/app/www/Data Test Technique V2.xlsx")
+  dta$Product <- factor(dta$Product)
+  dta$Judge <- factor(dta$Judge)
 
   p_dta <- reactive({
     dta
@@ -18,16 +20,27 @@ app_server <- function(input, output, session) {
     output$tbl_dta <- renderTable({p_dta()})
             )
 
-  #MODEL
-  observe(
+  #MODEL ANOVA
+  observe({
     #as.formula(paste0( ))
-    # resAov <- FactoMineR::AovSum(formula = `Sensory Variable 1` ~ Product + Judge + Product:Judge, data = p_dta())$Ftest,
-    # tbl_Factor <- rownames(resAov$Ftest),
-    # resAov <- resAov[,c(length(resAov[1,]),1:(length(resAov[1,])-1))],
-    # colnames(resAov)[1] <- str_to_upper(variables[i]),
-
+    resAov <- FactoMineR::AovSum(formula = `Sensory Variable 1` ~ Product + Judge, data = p_dta())$Ftest
+    resAov$` ` <- rownames(resAov)
+    resAov$`Pr(>F)` <- as.character(signif(resAov$`Pr(>F)`, digits = 3))
     output$main_res <- renderTable(
-      FactoMineR::AovSum(formula = `Sensory Variable 1` ~ Product + Judge + Product:Judge, data = p_dta())$Ftest
+      resAov %>% select(` `, everything())
       )
-  )
+  })
+
+
+  #ACP
+  # acp <- PCA(data, scale.unit = T)
+  # fviz_pca_ind(acp,
+  #              geom.ind = c("point","text"), #c("#FA0000", "#FFFFFF", "#FFFFFF") show points only (nbut not "text")
+  #              col.ind = as.factor(couleur)
+  # )
+  #
+  # fviz_pca_var(acp, col.var = "cos2",
+  #              gradient.cols = c("#FA0000", "#E7B800", "#5FCC00"),
+  #              repel = TRUE # Avoid text overlapping
+  # ) #alpha.var="cos2"
 }
